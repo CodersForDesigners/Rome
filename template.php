3ff126c5882f96bb139d19d786835ff2d3e0ff22
Mario Windsor <mario@lazaro.in>
2018-02-12 16:26:17 +0530

    updated template.php

--- template.php
+++ template.php
@@ -1,222 +1,161 @@
-
 <!DOCTYPE html>
-<html dir="ltr" lang="en-US">
-	<head>
+<html>
+<head>
 	<meta charset="utf-8" />
-	<title>Our Cats and All About Them</title>
-	<meta name="author" content="Harrison Weir"/>
-	<meta name="subject" content="Cats. Their Varieties, Habits and Management; and for show, the Standard of Excellence and Beauty"/>
-	<meta name="keywords" content="cats,feline"/>
-	<meta name="date" content="2014-12-05"/>
-
+	<title>Page Title</title>
 	<style type="text/css">
+		/**
+		 * Print Stylesheet fuer Deinewebsite.de
+		* @version         1.0
+		* @lastmodified    16.06.2016
+		*/
+
+		/*	-- 
+		***	Pixels => Points
+		*	6px => 5pt			*	11px => 8pt			*	16px => 12pt		*	21px => 16pt
+		*	7px => 5pt			*	12px => 9pt			*	17px => 13pt		*	22px => 17pt
+		*	8px => 6pt			*	13px => 10pt		*	18px => 14pt		*	23px => 17pt
+		*	9px => 7pt			*	14px => 11pt		*	19px => 14pt		*	24px => 18pt
+		*	10px => 8pt			*	15px => 11pt		*	20px => 15pt		
+		***	
+		--	*/
+
+		@media screen {
+			body > * { display: none; }
+			body:after {
+				content: 'Press: Command + P';
+			}
+
+
+			/* -- PRINT HELPER CLASSES -- */
+			.page-break	{ display: none; }
+		}
+
+		@media print {
 			
-	@page {
-		size: 5.5in 8.5in;
-		margin: 70pt 60pt 70pt;
-	}
+			/** Setting Page size and margins */
+			@page {
+				size: A4;
+				margin: 2cm;
+			}
 
-	@page:first {
-		size: 5.5in 8.5in;
-		margin: 0;
-	}
+			/* Set font to 16px/13pt, set background to white and font to black.*/
+			/* This saves ink */
+			body {
+				color: #000;
+				font-family: "Trebuchet MS", "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", Tahoma, sans-serif;
+				font-size: 13pt;
+				line-height: 1.3;
+				background: #fff !important;
+			}
+
+			h1 {
+				font-size: 24pt;
+			}
+
+			h2, h3, h4 {
+				font-size: 14pt;
+				margin-top: 25px;
+			}
+			 
+			/* Defining all page breaks */
+			a {
+				page-break-inside:avoid
+			}
+			blockquote {
+				page-break-inside: avoid;
+			}
+			h1, h2, h3, 
+			h4, h5, h6 { page-break-after:avoid; 
+				page-break-inside:avoid; }
+			img { page-break-inside:avoid; 
+				page-break-after:avoid; }
+			table, pre { page-break-inside:avoid; }
+			ul, ol, dl  { page-break-before:avoid; }
+			
+			/* Displaying link color and link behaviour */
+			a:link, a:visited, a {
+				background: transparent;
+				color: #520;
+				font-weight: bold;
+				text-decoration: underline;
+				text-align: left;
+			}
+
+			a {
+				page-break-inside:avoid;
+			}
+
+			a[href^=http]:after {
+				content:" <" attr(href) "> ";
+			}
+
+			$a:after > img {
+				content: "";
+			}
+
+			article a[href^="#"]:after {
+				content: "";
+			}
+
+			a:not(:local-link):after {
+				content:" <" attr(href) "> ";
+			}
 
 
-	div.frontcover {
-		page: cover;
-		content: url("images/cover.png");
-		width: 100%;
-		height: 100%;
-	}
+			/* Define Important Elements */    
+			p, address, li, dt, dd, blockquote {
+				font-size: 100%;
+			}
 
-	
-	@page:right{ 
-		@bottom-left {
-			margin: 10pt 0 30pt 0;
-			border-top: .25pt solid #666;
-			content: "Our Cats";
-			font-size: 9pt;
-			color: #333;
+			/* Font for Code Samples */
+			code, pre { font-family: "Courier New", Courier, mono}
+
+			ul, ol {
+				list-style: square; 
+				margin-left: 18pt;
+				margin-bottom: 20pt;    
+			}
+
+			li {
+				line-height: 1.6em;
+			}
+
+
+
+			/* -- PRINT HELPER CLASSES -- */
+			.page-break {
+				display: block;
+				width: 100%;
+				page-break-after: always;
+			}
 		}
-
-		@bottom-right { 
-			margin: 10pt 0 30pt 0;
-			border-top: .25pt solid #666;
-			content: counter(page);
-			font-size: 9pt;
-		}
-
-		@top-right {
-			content:  string(doctitle);
-			margin: 30pt 0 10pt 0;
-			font-size: 9pt;
-			color: #333;
-		}
-	}
-
-	
-	@page:left {
-		@bottom-right {
-			margin: 10pt 0 30pt 0;
-			border-top: .25pt solid #666;
-			content: "Our Cats";
-			font-size: 9pt;
-			color: #333;
-		}
-
-		@bottom-left { 
-			margin: 10pt 0 30pt 0;
-			border-top: .25pt solid #666;
-			content: counter(page);
-			font-size: 9pt;
-		}
-	}
-
-
-	@page:first {
-		@bottom-right {
-			content: normal;
-			margin: 0;
-		}
-
-		@bottom-left {
-			content: normal;
-			margin: 0;
-		}
-	}
-
-
-	/* Reset chapter and figure counters on the body */
-	body {
-		counter-reset: chapternum figurenum;
-		font-family: "Trebuchet MS", "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", Tahoma, sans-serif;
-		line-height: 1.5;
-		font-size: 11pt;
-	}
-
-	/* Get the title of the current chapter, which will be the content of the h1.
-	Reset figure counter because figures start from 1 in each chapter. */
-	h1 {
-		string-set: doctitle content();
-		page-break-before: always;
-		counter-reset: figurenum;
-		counter-reset: footnote;
-		line-height: 1.3;
-	}
-
-	/* Increment chapter counter */
-	h1.chapter:before {
-		counter-increment: chapternum;
-		content: counter(chapternum) ". ";
-	}
-
-	/* Increment and display figure counter */
-	figcaption:before {
-		counter-increment: figurenum;
-		content: counter(chapternum) "-" counter(figurenum) ". ";
-	}
-
-
-	.fn {
-		float: footnote;
-	}
-
-	.fn {
-		counter-increment: footnote;
-	}
-
-	.fn::footnote-call {
-		content: counter(footnote);
-		font-size: 9pt;
-		vertical-align: super;
-		line-height: none;
-	}
-
-	.fn::footnote-marker {
-		font-weight: bold;
-	}
-
-	@page {
-		@footnote {
-			border-top: 0.6pt solid black;
-			padding-top: 8pt;
-		}
-	}
-
-
-	h1, h2, h3, h4, h5 {
-		font-weight: bold;
-		page-break-after: avoid;
-		page-break-inside:avoid;
-	}
-
-	h1+p, h2+p, h3+p {
-		page-break-before: avoid;
-	}
-
-	table, figure {
-		page-break-inside: avoid;
-	}
-
-
-	ul.toc a::after {
-		content: target-counter(attr(href), page);
-	}
-
-
-	ul.toc a::after {
-		content: leader('.') target-counter(attr(href), page);
-	}
-
-
 	</style>
-	</head>
-	<body>
-		<div class="frontcover">
-		</div>
-		<div class="contents">
-			<h1>Extracts from Our Cats and All About Them by Harrison Weir</h1>
+</head>
+<body>
 
-				<ul class="toc">
-					<li><a href="#ch1">The First Cat Show</a></li>
-					<li><a href="#ch2">Trained Cats</a></li>
-					<li><a href="#ch3">General Management</a></li>
-					<li><a href="#ch4">Superstition and Witchcraft</a></li>
-				</ul>
-
-		</div>
-
-		<h1 id="ch1" class="chapter">The First Cat Show</h1>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-		<h1 id="ch2" class="chapter">Trained Cats</h1>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-		<h1 id="ch3" class="chapter">General Management</h1>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-		<h1 id="ch4" class="chapter">Superstition and Witchcraft</h1>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-	</body>
+	<?php
+		$unit_number = 201;
+		$area = 2400;
+		$rate_per_sqft = 3900;
+		$total = 9360000;
+	?>
+	<div class="page">
+		<h1>Unit Number: <?php echo $unit_number; ?></h1>
+		<h2>Area: <?php echo $area; ?> SQFT</h2>
+		<h2>Rate/SQFT: ₹<?php echo $rate_per_sqft; ?></h2>
+		<h2>Total: ₹<?php echo $total; ?></h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+	<div class="page-break"></div>
+	<div class="page">
+		<h1>Pricing Details</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+	<div class="page-break"></div>
+	<div class="page">
+		<h1>Pricing Details</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+</body>
 </html>
