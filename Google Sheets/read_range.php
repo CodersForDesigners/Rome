90b9fb0edcc3546cd6edc910cd15642f58b3dc49
adi <adityabhat@lazaro.in>
2018-02-14 10:44:23 +0530

    Google Sheet prototypes; move them to a dedicated folder

--- Google Sheets/read_range.php
+++ Google Sheets/read_range.php
@@ -3,7 +3,7 @@
 ini_set( 'display_errors', 'On' );
 ini_set( 'error_reporting', E_ALL );
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 date_default_timezone_set( 'Asia/Kolkata' );
 
@@ -14,19 +14,14 @@ function logo ( $thing ) {
 }
 
 define( 'SPREADSHEET_ID', '1LupSf0NLpR4Qtw-Nwpok0NCR7BcZWGME1AjOxPf1WGU' );
-define( 'SHEET_RANGE', 'Sheet1!I2' );
-$requestBody = new Google_Service_Sheets_ValueRange( [
-	'range' => 'Sheet1!I2',
-	'majorDimension' => 'ROWS',
-	'values' => [ 'values' => 'E92' ]
-] );
+define( 'SHEET_RANGE', 'calculations!A1:J' );
 
 define( 'APPLICATION_NAME', 'This is a Test' );
 define( 'CREDENTIALS_PATH', '~/.credentials/sheets.googleapis.com-php-quickstart.json' );
 define( 'CLIENT_SECRET_PATH', __DIR__ . '/client_secret.json' );
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/sheets.googleapis.com-php-quickstart.json
-define( 'SCOPES', implode( ' ', [ Google_Service_Sheets::SPREADSHEETS ] ) );
+define( 'SCOPES', implode( ' ', [ Google_Service_Sheets::SPREADSHEETS_READONLY ] ) );
 
 // only permit this to run from the command line
 // if ( php_sapi_name() != 'cli' ) {
@@ -91,17 +86,11 @@ function expandHomeDirectory ( $path ) {
 
 // Get the API client and construct the service object.
 $client = getClient();
-$service = new Google_Service_Sheets($client);
+$service = new Google_Service_Sheets( $client );
 
 // $spreadsheetId = '1LupSf0NLpR4Qtw-Nwpok0NCR7BcZWGME1AjOxPf1WGU';
 // $range = 'Sheet1!A1:J';
-$response = $service->spreadsheets_values->update(
-	SPREADSHEET_ID,
-	SHEET_RANGE,
-	$requestBody,
-	[
-		'valueInputOption' => 'USER_ENTERED'
-	]
-);
+$response = $service->spreadsheets_values->get( SPREADSHEET_ID, SHEET_RANGE );
+$values = $response->getValues();
 
-logo( $response );
+logo( $values );
--- Google Sheets/write_range.php
+++ Google Sheets/write_range.php
@@ -1,6 +1,9 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+ini_set( 'display_errors', 'On' );
+ini_set( 'error_reporting', E_ALL );
+
+require_once __DIR__ . '/../vendor/autoload.php';
 
 date_default_timezone_set( 'Asia/Kolkata' );
 
@@ -11,14 +14,19 @@ function logo ( $thing ) {
 }
 
 define( 'SPREADSHEET_ID', '1LupSf0NLpR4Qtw-Nwpok0NCR7BcZWGME1AjOxPf1WGU' );
-define( 'SHEET_RANGE', 'Sheet1!A1:J' );
+define( 'SHEET_RANGE', 'calculations!I2' );
+$requestBody = new Google_Service_Sheets_ValueRange( [
+	'range' => SHEET_RANGE,
+	'majorDimension' => 'ROWS',
+	'values' => [ 'values' => 'A21' ]
+] );
 
 define( 'APPLICATION_NAME', 'This is a Test' );
 define( 'CREDENTIALS_PATH', '~/.credentials/sheets.googleapis.com-php-quickstart.json' );
 define( 'CLIENT_SECRET_PATH', __DIR__ . '/client_secret.json' );
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/sheets.googleapis.com-php-quickstart.json
-define( 'SCOPES', implode( ' ', [ Google_Service_Sheets::SPREADSHEETS_READONLY ] ) );
+define( 'SCOPES', implode( ' ', [ Google_Service_Sheets::SPREADSHEETS ] ) );
 
 // only permit this to run from the command line
 // if ( php_sapi_name() != 'cli' ) {
@@ -39,7 +47,7 @@ function getClient () {
 	// Load previously authorized credentials from a file.
 	$credentialsPath = expandHomeDirectory( CREDENTIALS_PATH );
 	if ( file_exists( $credentialsPath ) ) {
-		$accessToken = json_decode(file_get_contents($credentialsPath), true);
+		$accessToken = json_decode( file_get_contents( $credentialsPath ), true );
 	} else {
 		// Request authorization from the user.
 		$authUrl = $client->createAuthUrl();
@@ -83,11 +91,17 @@ function expandHomeDirectory ( $path ) {
 
 // Get the API client and construct the service object.
 $client = getClient();
-$service = new Google_Service_Sheets($client);
+$service = new Google_Service_Sheets( $client );
 
 // $spreadsheetId = '1LupSf0NLpR4Qtw-Nwpok0NCR7BcZWGME1AjOxPf1WGU';
 // $range = 'Sheet1!A1:J';
-$response = $service->spreadsheets_values->get( SPREADSHEET_ID, SHEET_RANGE );
-$values = $response->getValues();
+$response = $service->spreadsheets_values->update(
+	SPREADSHEET_ID,
+	SHEET_RANGE,
+	$requestBody,
+	[
+		'valueInputOption' => 'USER_ENTERED'
+	]
+);
 
-logo( $values );
+logo( $response );
